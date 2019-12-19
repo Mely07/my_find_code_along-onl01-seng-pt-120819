5a2f7d5c711430a95c74d8eb5b23230b0187636e
Damely Tineo <damely.tineo07@gmail.com>
2019-12-19 03:56:34 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,26 +1,10 @@
-require 'pry'
 
 def my_find(collection)
   i = 0
   while i < collection.length
-   yield(collection[i])
+    return collection[i] if yield(collection[i])
     i = i + 1
   end
 end
 
 
-
-def my_all?(collection)
-  i = 0
-  block_return_values = []
-  while i < collection.length
-    block_return_values << yield(collection[i])
-    i = i + 1
-  end
- 
-  if block_return_values.include?(false)
-    false
-  else
-    true
-  end
-end
