7e6f19ff6090483a85d35f19059937bd3858c5fc
Damely Tineo <damely.tineo07@gmail.com>
2019-12-19 03:56:24 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,12 +2,13 @@ require 'pry'
 
 def my_find(collection)
   i = 0
-   = []
   while i < collection.length
-     << yield(collection[i])
+   yield(collection[i])
     i = i + 1
   end
 end
+
+
 
 def my_all?(collection)
   i = 0
